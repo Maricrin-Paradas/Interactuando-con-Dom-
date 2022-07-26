47700b423d4ae0e96d02dd44faeb53d12dc4ff81
MariParadas <maaricrin@gmail.com>
2022-07-25 23:27:36 -0400

    practica con operador avanzado

--- js.js
+++ js.js
@@ -1,32 +1,59 @@
+let pruebaTejido = 0
+
 class crochet {
   constructor(material, hilos, accesorios) {
     this.material = material;
     this.hilos = hilos;
     this.accesorios = accesorios;
   }
+
 }
 
-let getMaterial = prompt("indica que material");
-let getHilo = prompt("indica el hilo");
-let getAccesorio = prompt("indica cual accesorio a usar");
+const getMaterial = document.getElementById("inputMaterial").value;
+const getHilo = document.getElementById("inputHilo").value;
+const getAccesorio = document.getElementById("inputAccesorio").value;
 
 const tejido1 = new crochet("2 rollo", "lana", "ganchillo");
 const tejido2 = new crochet("1 rollo", "algodon", "palillo");
 const tejido3 = new crochet("24 rollo", "sintetico", "ganchillo");
+const tejido4 = {
+  material:undefined,
+  hilo:undefined,
+  accesorios:undefined
+}
+
+const copyTejido4 = {
+  ...tejido4
+}
+
+console.log(copyTejido4);
 
 let misTejidos = [];
 
-misTejidos.push(tejido1, tejido2, tejido3);
+misTejidos.push(tejido1, tejido2, tejido3, tejido4);
 
-const agregarTejidos = (material, hilo, accesorio) => {
+console.table(misTejidos)
+
+/*const agregarTejidos = (material, hilo, accesorio) => {
   const tejido4 = new crochet(material, hilo, accesorio);
   misTejidos.push(tejido4);
   console.log(misTejidos);
-};
+};*/
 
-agregarTejidos(getMaterial, getHilo, getAccesorio);
+console.table(getMaterial, getHilo, getAccesorio);
 
-const divCrochet = document.getElementById("divCrochet");
+let {material, hilos, accesorios} = tejido1
+console.log(material)
+console.log(hilos)
+console.log(accesorios)
+
+pruebaTejido += tejido4.hilo ?? 0
+pruebaTejido += tejido4.material ?? 0
+pruebaTejido += tejido4.accesorios ?? 0
+
+console.log(pruebaTejido)
+
+/*const divCrochet = document.getElementById("divCrochet");
 
 misTejidos.forEach((crochet) => {
   divCrochet.innerHTML += `
@@ -36,4 +63,4 @@ misTejidos.forEach((crochet) => {
     <p>accesorios: ${crochet.accesorios} </p>
     </div>
   `;
-});
+});*/
